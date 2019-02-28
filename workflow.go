8a2665c58497a106303c866656134f1f84e5d86b
Jason.Yu <morty.yu@yahoo.com>
2019-02-28 16:45:04 +0800

    workflow test new

--- workflow.go
+++ workflow.go
@@ -1,14 +1,42 @@
 package giida
 
+import (
+  "reflect"
+)
+
 type Workflow struct {
   name string
-  goroutineTasks  chan struct{}
+  capacity int
+  procs map[string]interface{}
+  inPorts map[string]port
+  outPorts map[string]port
+  connections []connection
+}
+
+type port struct {
+  proc string
+  port string
+  channel reflect.Value
+}
+
+type portName struct {
+  proc string
+  port string
+}
+
+type connection struct {
+  src portName
+  tgt portName
+  channel reflect.Value
 }
 
 func NewWorkflow(name string, maxGoroutineTasks int) *Workflow {
   wf := &Workflow{
     name: name,
-    goroutineTasks: make(chan struct{}, maxGoroutineTasks),
+    capacity: maxGoroutineTasks,
+    inPorts:  make(map[string]port, maxGoroutineTasks),
+    outPorts: make(map[string]port, maxGoroutineTasks),
+    connections:  make([]connection, 0, maxGoroutineTasks),
   }
   return wf
 }
--- workflow_test.go
+++ workflow_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestSetWfName(t *testing.T) {
-  wf := NewWorkflow("workflowname", 4)
+  wf := NewWorkflow("workflowname", 8)
 
   expectedWfName := "workflowname"
   if wf.name != expectedWfName {
@@ -13,10 +13,76 @@ func TestSetWfName(t *testing.T) {
   }
 }
 
-func TestMaxGoroutineCapacity(t *testing.T) {
-  wf := NewWorkflow("tmp", 16)
+func newDoubleEcho() (*Workflow, error) {
+  n := NewWorkflow()
+  // Process
+  e1 := new(echo)
+  e2 := new(echo)
 
-  if got := cap(wf.goroutineTasks); got != 16 {
-    t.Errorf("Workflow goroutine cap number is %d, expect 16", got)
+  if err := n.Add("e1", e1); err != nil {
+    return nil, err
+  }
+  if err := n.Add("e2", e2); err != nil {
+    return nil, err
+  }
+  // Ports
+  if err := n.MapInPort("netIn", "e1", "In"); err != nil {
+    return nil, err
+  }
+  if err := n.MapOutPort("netOut", "e2", "Out"); err != nil {
+    return nil, err
+  }
+
+  return n, nil
+}
+
+func TestSimpleWorkflow(t *testing.T) {
+  n, err := newDoubleEcho()
+  if err != nil {
+    t.Error(err)
+    return
+  }
+
+  testWorkflowWithNumberSequence(n, t)
+}
+
+func testWorkflowWithNumberSequence(n *Workflow, t *testing.T) {
+  data := []int{93, 52, 1, 24, 35, 63, 634,12}
+
+  in := make(chan int)
+  out := make(chan int)
+  n.SetInPort("netIn", in)
+  n.SetOutPort("netOut", out)
+
+  wait := Run(n)
+
+  go func() {
+    for _, n := range data {
+      in <- n
+    }
+    close(in)
+  }()
+
+  i := 0
+  for got := range out {
+    expected := data[i]
+    if got != expected {
+      t.Errorf("%d != %d\n", got, expected)
+    }
+    i++
+  }
+
+  <- wait
+}
+
+// Process for test
+type echo struct {
+  In <-chan int
+  Out chan<- int
+}
+
+func (c *echo) Task() {
+  for i := range c.In {
+    c.Out <- i
   }
 }
