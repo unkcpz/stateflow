1a1ca588d383254e7bfba00f6ac9b85d4091ba9c
Jason.Yu <morty.yu@yahoo.com>
2019-03-06 15:15:05 +0800

    once run process

--- process.go
+++ process.go
@@ -5,13 +5,6 @@ type Tasker interface {
   Execute()
 }
 
-// type Process struct {
-//   Name string
-//   ID  int
-//   State   ENUM
-//   Task  Tasker
-// }
-
 // Done notifies that the Execute is finished
 type Done struct{}
 
@@ -28,25 +21,30 @@ func Run(c Tasker) Wait {
   return wait
 }
 
-type InputGuard struct {
-  ports map[string]bool
-  complete int
+type Process struct {
+  Name string
+  Task  Tasker
 }
 
+// type InputGuard struct {
+//   ports map[string]bool
+//   complete int
+// }
 //
-func NewInputGuard(ports ...string) *InputGuard {
-  portMap := make(map[string]bool, len(ports))
-  for _, p := range ports {
-    portMap[p] = false
-  }
-  return &InputGuard{portMap, 0}
-}
-
-// Complete is called when a port is closed and returns true when all the ports have been closed
-func (g *InputGuard) Complete(port string) bool {
-  if !g.ports[port] {
-    g.ports[port] = true
-    g.complete++
-  }
-  return g.complete >= len(g.ports)
-}
+// //
+// func NewInputGuard(ports ...string) *InputGuard {
+//   portMap := make(map[string]bool, len(ports))
+//   for _, p := range ports {
+//     portMap[p] = false
+//   }
+//   return &InputGuard{portMap, 0}
+// }
+//
+// // Complete is called when a port is closed and returns true when all the ports have been closed
+// func (g *InputGuard) Complete(port string) bool {
+//   if !g.ports[port] {
+//     g.ports[port] = true
+//     g.complete++
+//   }
+//   return g.complete >= len(g.ports)
+// }
--- process_test.go
+++ process_test.go
@@ -4,25 +4,6 @@ import (
   "testing"
 )
 
-// Test a simple Task that runs only once
-func TestSimpleTask(t *testing.T) {
-  in := make(chan int)
-  out := make(chan int)
-  proc := &doubleOnce{
-    in,
-    out,
-  }
-
-  wait := Run(proc)
-
-  in <- 21
-  if got := <-out; got != 42 {
-    t.Errorf("%d != %d", got, 42)
-  }
-
-  <-wait
-}
-
 type doubleOnce struct {
   In <-chan int
   Out chan<- int
@@ -33,126 +14,121 @@ func (proc *doubleOnce) Execute() {
   proc.Out <- 2 * i
 }
 
-// Test a simple long running Task with one input
-func TestSimpleLongRunningTask(t *testing.T) {
+// Test a simple task with multi inputs
+func TestSimpleMultiTask(t *testing.T) {
   tests := []struct {
     in int
     expected int
   }{
     {12, 24},
     {5, 10},
+    {0, 0},
   }
-
-  in := make(chan int)
-  out := make(chan int)
-  proc := &doubler{
-    in,
-    out,
-  }
-
-  wait := Run(proc)
 
   for _, test := range tests {
-    in <- test.in
-    got := <-out
-
-    if got != test.expected {
-      t.Errorf("%d != %d", got, test.expected)
+    in := make(chan int)
+    out := make(chan int)
+    proc := &doubleOnce{
+      in,
+      out,
     }
-  }
 
-  close(in)
-  <-wait
-}
+    wait := Run(proc)
+    in <- test.in
+    if got := <-out; got != test.expected {
+      t.Errorf("%d * 2 != %d", test.in, got)
+    }
 
-type doubler struct {
-  In <-chan int
-  Out chan<- int
-}
-
-func (proc *doubler) Execute() {
-  for i := range proc.In {
-    proc.Out <- 2 * i
+    <-wait
   }
 }
 
 func TestTaskWithTwoInputs(t *testing.T) {
   tests := []struct {
-    op1 int
-    op2 int
-    sums int
+    x int
+    y int
+    sum int
   }{
     {3, 38, 41},
     {3, 4, 7},
     {92, 4, 96},
+    {-1, 1, 0},
   }
-
-  in1 := make(chan int)
-  in2 := make(chan int)
-  out := make(chan int)
-  c := &adder{in1, in2, out}
-
-  wait := Run(c)
-
-  go func() {
-    for _, t := range tests {
-      in1 <-t.op1
-    }
-    close(in1)
-  }()
-  go func() {
-    for _, t := range tests {
-      in2 <-t.op2
-    }
-    close(in2)
-  }()
 
   for _, test := range tests {
-    got := <-out
-    expected := test.sums
-    if got != expected {
-      t.Errorf("%d + %d = %d, expect %d", test.op1, test.op2, got, expected)
-    }
-  }
+    x := make(chan int)
+    y := make(chan int)
+    sum := make(chan int)
 
-  <-wait
+    proc := &adder{
+      x,
+      y,
+      sum,
+    }
+
+    wait := Run(proc)
+
+    x <- test.x
+    y <- test.y
+    got := <-sum
+    if got != test.sum {
+      t.Errorf("%d + %d != %d", test.x, test.y, test.sum)
+    }
+    <- wait
+  }
 }
 
 type adder struct {
-  Op1 <-chan int
-  Op2 <-chan int
+  X <-chan int
+  Y <-chan int
   Sum chan<- int
 }
 
-func (c *adder) Execute() {
-  guard := NewInputGuard("op1", "op2")
-
-  op1Buf := make([]int, 0, 10)
-  op2Buf := make([]int, 0, 10)
-  addOp := func(op int, buf, otherBuf *[]int) {
-    if len(*otherBuf) > 0 {
-      otherOp := (*otherBuf)[0]
-      *otherBuf = (*otherBuf)[1:]
-      c.Sum <- (op + otherOp)
-    } else {
-      *buf = append(*buf, op)
-    }
-  }
-
-  for {
-    select {
-    case op1, ok := <-c.Op1:
-      if ok {
-        addOp(op1, &op1Buf, &op2Buf)
-      } else if guard.Complete("op1") {
-        return
-      }
-    case op2, ok := <-c.Op2:
-      if ok {
-        addOp(op2, &op2Buf, &op1Buf)
-      } else if guard.Complete("op2") {
-        return
-      }
-    }
-  }
+func (p *adder) Execute() {
+  x := <-p.X
+  y := <-p.Y
+  p.Sum <- x + y
 }
+
+// // Test a simple long running Task with one input
+// func TestSimpleLongRunningTask(t *testing.T) {
+//   tests := []struct {
+//     in int
+//     expected int
+//   }{
+//     {12, 24},
+//     {5, 10},
+//   }
+//
+//   in := make(chan int)
+//   out := make(chan int)
+//   proc := &doubler{
+//     in,
+//     out,
+//   }
+//
+//   wait := Run(proc)
+//
+//   for _, test := range tests {
+//     in <- test.in
+//     got := <-out
+//
+//     if got != test.expected {
+//       t.Errorf("%d != %d", got, test.expected)
+//     }
+//   }
+//
+//   close(in)
+//   <-wait
+// }
+//
+// type doubler struct {
+//   In <-chan int
+//   Out chan<- int
+// }
+//
+// func (proc *doubler) Execute() {
+//   for i := range proc.In {
+//     proc.Out <- 2 * i
+//   }
+// }
